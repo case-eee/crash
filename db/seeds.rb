f56ef62fe11352e704039e0085cc0d2ae7158198
Casey Ann Cumbow <cumbow8@gmail.com>
2016-01-17 11:30:27 -0600

    adds some basic seed data

---
+++ db/seeds.rb
@@ -0,0 +1,39 @@
+"Grand & Western Donation Drop Box"
+"525 N Western Ave"
+"Chicago, IL, 60612"
+
+"Clybourn Ave Family Store & Donation Center"
+"2270 N Clybourn"
+"Chicago, IL, 60614"
+
+"Salvation Army Temple Corps Donation Drop Box"
+"1 N Ogden Ave"
+"Chicago, IL, 60607"
+
+"N Union St. Family Store & Donation Center"
+"509 N Union"
+"Chicago, IL, 60654"
+
+"West Humboldt Park Family Store & Donation Center"
+"4052 W Grand Ave"
+"Chicago, IL, 60651"
+
+"Clark St Drop Box"
+"1730 N Clark St"
+"Chicago, IL, 60614"
+
+"Salvation Army La Villita Corps Donation Drop Box"
+"3621 W 24th St"
+"Chicago, IL, 60623"
+
+"Montrose Ave Family Store & Donation Center"
+"3301 W Montrose Ave."
+"Chicago, IL, 60618"
+
+"Broadway Ave Family Store & Donation Center"
+"4315 N Broadway Ave."
+"Chicago, IL, 60613"
+
+
+
+
